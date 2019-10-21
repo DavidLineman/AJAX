e58bbf7f3f2d8223cce5e55f4a81d500ced4676e
DavidLineman <dline215@gmail.com>
2019-10-21 15:00:45 -0400

    worked on adding breed list to select via api call, still working on it

--- doggos.js
+++ doggos.js
@@ -1,6 +1,23 @@
 const DOG_URL = "https://dog.ceo/api/breeds/image/random";
+const breedList = "https://dog.ceo/api/breeds/list/all";
 
 const doggos = document.querySelector(".doggos");
+const doggoList = document.querySelector(".breed");
+
+
+function createDoggoList() {
+  const list = fetch(breedList);
+  list
+    .then(function (response) {
+      const processingList = response.json();
+      return processingList;
+    })
+    .then(function (processedResponse) {
+      const option = document.createElement("option");
+      option.src = processedResponse.message;
+      doggoList.appendChild(option); 
+    }); 
+}
 
 
 function addNewDoggo() {
@@ -19,5 +36,6 @@ function addNewDoggo() {
 }
 
 
-document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
 
+document.querySelector(".add-doggo").addEventListener("click", addNewDoggo);
+document.querySelector(".breed").addEventListener("click", createDoggoList);
